f143a5a343341a629b1d7758334ac4e0eaf5bbce
PROF ROMULO <rjlpena@gmail.com>
2023-10-02 16:13:46 -0300

    round

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomCapcom(){
     document.querySelector("#som_tecla_capcom").play();
 }
 document.querySelector(".tecla_capcom").onclick = tocaSomCapcom;
+
+function tocaSomRound(){
+    document.querySelector("#som_tecla_round").play();
+}
+document.querySelector(".tecla_capcom").onclick = tocaSomRound;
