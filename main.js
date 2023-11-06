b312aae8ec65eb63fd3296e68cf23b9ad8726a38
PROF ROMULO <rjlpena@gmail.com>
2023-11-06 18:23:40 +0000

    eventos

--- main.js
+++ main.js
@@ -11,6 +11,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
+    tecla.onkeydown = function(evento){
+        if (evento.code != 'Tab'){
+            tecla.classList.add('ativa');
+        }    
+
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 
 }
 
